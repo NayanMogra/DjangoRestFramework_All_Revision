4e9cb31c71eb8939b5b0278d8bffc23251bfe76b
NayanMogra <nayanmogra@gmail.com>
2021-04-09 14:40:56 +0530

    Delete restframework/modelserializer directory

--- restframework/modelserializer/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from basicserializers.models import Student
-
-class StudentsSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Student
-        fields = '__all__'
--- restframework/modelserializer/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import *
-urlpatterns = [
-    path('' , students_details , name = 'studentsdetails'),
-    path('<int:id>' , student_detail , name = 'studentdetail')
-]
